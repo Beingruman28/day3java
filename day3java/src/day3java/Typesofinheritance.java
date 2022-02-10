d18205953dbedfcf9c5bb269b11b0f0b48c12ad6
Ruman <mohammedruman1996@gmail.com>
2022-02-10 14:47:13 +0530

    practiced the commit!

--- day3java/src/day3java/Typesofinheritance.java
+++ day3java/src/day3java/Typesofinheritance.java
@@ -1,7 +1,18 @@
 package day3java;
 
+public class Typesofinheritance extends Parent1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Parent3 P3 = new Parent3();	
+      P3.sum(); 
+      Parent1 p1 = new Parent1();
+      p1.sum(); 
+	}
+}
+
 class Parent1{ // parent 
 	void sum() {
+	System.out.println("hi this is a parent");
 }
 }
 class Parent2 extends Parent1{ //child 1 of parent 1
@@ -10,11 +21,4 @@ class Parent3 extends Parent2{ //child2
  void sum() {
 	 System.out.println("program successfull!");
 }
-}
- public class Typesofinheritance extends Parent1 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       Parent3 P3 = new Parent3();	
-      P3.sum(); 
-	}
 }
